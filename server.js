f2516a561c97e9a5355408de7f75e11cda43bf91
LAPTOP-5NC2RQ85\Calvin <calvinhyleung@gmail.com>
2021-06-04 17:34:01 -0400

    post typo

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(function(req, res, next) {
     } else if ('GET' === req.method) {
         //respond with 200
         res.send("success");
-    } else if ('GET' === req.method) {
+    } else if ('POST' === req.method) {
         //respond with 200
         res.send("success");
         var data = request.body;
