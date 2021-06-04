607f66413cce88bc4d3ccf4e8a8ed6aa059e6ff2
LAPTOP-5NC2RQ85\Calvin <calvinhyleung@gmail.com>
2021-06-04 18:04:12 -0400

    remove cors

--- server.js
+++ server.js
@@ -19,79 +19,79 @@ function listening(){
 // use Morgan to log requests
 app.use(logger("short"));
 
-//options request 
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+// //options request 
+// app.use(function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-    //intercepts OPTIONS method
-    if ('OPTIONS' === req.method) {
-      res.send("success");
-    } else if ('GET' === req.method) {
-        res.send("success");
-    } else if ('POST' === req.method) {
-        res.send("success");
-        var data = req.body;
-        console.log(data);
-        var first_name = data.first_name;
-        var last_name = data.last_name;
-        var email = data.email;
-        var papercut_num = data.papercut_num;
-        var cost = data.cost;
-        var today = new Date();
-        var dd = String(today.getDate()).padStart(2, '0');
-        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-        var yyyy = today.getFullYear();
-        today = mm + '/' + dd + '/' + yyyy;
-        //var productType = data.productType;
-        //var status = data.status; 
-        var replyFull
-            = "Last:  " + last_name + "\n" 
-            + "First: " + first_name + "\n"  
-            + "PC #:  " + papercut_num + "\n" 
-            + "Cost:  $" + cost + "\n" 
-            + "Date:  " + today + "\n" 
-            + "Email: " + "\n" 
-            + email ;
-        console.log("\nJSON received\n");
-        console.log(data);
-        console.log("\n");
-        response.send("\nPOST printing label...");
-        fs.writeFile('label.txt', replyFull, function (err) {
-            if (err) return console.log(err);
-            console.log('POST Writing information to label.txt');
-            console.log(replyFull);
+//     //intercepts OPTIONS method
+//     if ('OPTIONS' === req.method) {
+//       res.send("success");
+//     } else if ('GET' === req.method) {
+//         res.send("success");
+//     } else if ('POST' === req.method) {
+//         res.send("success");
+//         var data = req.body;
+//         console.log(data);
+//         var first_name = data.first_name;
+//         var last_name = data.last_name;
+//         var email = data.email;
+//         var papercut_num = data.papercut_num;
+//         var cost = data.cost;
+//         var today = new Date();
+//         var dd = String(today.getDate()).padStart(2, '0');
+//         var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+//         var yyyy = today.getFullYear();
+//         today = mm + '/' + dd + '/' + yyyy;
+//         //var productType = data.productType;
+//         //var status = data.status; 
+//         var replyFull
+//             = "Last:  " + last_name + "\n" 
+//             + "First: " + first_name + "\n"  
+//             + "PC #:  " + papercut_num + "\n" 
+//             + "Cost:  $" + cost + "\n" 
+//             + "Date:  " + today + "\n" 
+//             + "Email: " + "\n" 
+//             + email ;
+//         console.log("\nJSON received\n");
+//         console.log(data);
+//         console.log("\n");
+//         response.send("\nPOST printing label...");
+//         fs.writeFile('label.txt', replyFull, function (err) {
+//             if (err) return console.log(err);
+//             console.log('POST Writing information to label.txt');
+//             console.log(replyFull);
             
-        });
-        //
-        //run shell command 
-        //if connected to printer, use the following command 
-        //"cupsenable DYMO_LabelWriter_450_Turbo && lp label.txt"
-        exec("lp label.txt", (error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                return;
-            }
-            console.log(`stdout: ${stdout}`);
-        });
-    }
-    // else {
-    // //move on
-    //   next();
-    // }
-});
+//         });
+//         //
+//         //run shell command 
+//         //if connected to printer, use the following command 
+//         //"cupsenable DYMO_LabelWriter_450_Turbo && lp label.txt"
+//         exec("lp label.txt", (error, stdout, stderr) => {
+//             if (error) {
+//                 console.log(`error: ${error.message}`);
+//                 return;
+//             }
+//             if (stderr) {
+//                 console.log(`stderr: ${stderr}`);
+//                 return;
+//             }
+//             console.log(`stdout: ${stdout}`);
+//         });
+//     }
+//     // else {
+//     // //move on
+//     //   next();
+//     // }
+// });
 
-app.options("/*", function(req, res, next){
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    res.send("options 2s");
-  });
+// app.options("/*", function(req, res, next){
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+//     res.send("options 2s");
+//   });
 
 // use express.json()
 app.use(express.json());
